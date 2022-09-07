18efffc44dc3e997b349624ff33ca8d9c85f4862
Anjanique Mackey <anjaniquemackey@gmail.com>
2022-09-07 13:28:08 -0700

    set up project, added stylesheet

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+function App() {
+  /*
+  i will need: (1) badge form (2)badge component underneath badge form, and a state thta holds each response as an array of objects.
+  !! each filed will have a minimum of 3 charecters requirement: 
+  all fields are required: add a required attribute for each input
+  !! make sure value of the phone # field is all digits
+  !! make each badge have an alternating color: I'm thinking they'll have an alternating accent color.
+  */
+  return (
+    <main>
+
+    </main>
+  );
+}
+
+export default App;
